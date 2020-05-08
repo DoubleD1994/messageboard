7d484e6548bc4b851406b1687d1457fde12d2464
ddr14 <david.dryburgh@sky.uk>
2020-05-08 12:34:45 +0100

    first commit

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MessagesComponent } from './messages.component';
+import {WebService} from './web.service';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MessagesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    HttpClientModule
+  ],
+  providers: [
+    WebService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
